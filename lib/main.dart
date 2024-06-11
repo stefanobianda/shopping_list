698d3dbd8fd38f2b0b9d533ce662ac8d3b851ebf
Stefano Bianda <stefanobianda@ik.me>
2024-06-11 16:58:45 +0200

    create the GroceryList widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Groceries',
-      theme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 147, 229, 250),
-          brightness: Brightness.dark,
-          surface: const Color.fromARGB(255, 42, 51, 59),
+        title: 'Flutter Groceries',
+        theme: ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 147, 229, 250),
+            brightness: Brightness.dark,
+            surface: const Color.fromARGB(255, 42, 51, 59),
+          ),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
         ),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
-      ),
-      home: ...,
-    );
+        home: const GroceryList());
   }
 }
-
-
-
