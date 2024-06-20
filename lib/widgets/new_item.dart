e7890ce6c5b6b0de979c2149c193080830577c4b
Stefano Bianda <stefanobianda@ik.me>
2024-06-20 08:42:26 +0200

    improve performance by avoid unnecessary http request

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -42,10 +43,16 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+
+      final Map<String, dynamic> resData = json.decode(response.body);
       if (!context.mounted) {
         return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
     }
   }
 
